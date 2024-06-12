c03e3e305fb85242f93c57fbf62200f78ed490d4
Minh Quân <nongminhquanqu@gmail.com>
2024-06-13 00:53:09 +0700

    Update blood.py

--- blood.py
+++ blood.py
@@ -1966,7 +1966,7 @@ def welcome():
 if __name__ == '__main__':
     init(convert=True)
     if len(sys.argv) < 2:
-        ua = open('./resources/ua.txt', 'r').read().split('\n')
+        ua = open('./ua.txt', 'r').read().split('\n')
         clr()
         welcome()
     elif len(sys.argv) == 5:
